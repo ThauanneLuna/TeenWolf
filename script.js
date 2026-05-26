c4fcb914254b1afcd3da0f7ad556fdd9fb66fed1
Thauanne Luna <thauluna.acessorios@gmail.com>
2026-05-26 12:30:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -488,20 +488,27 @@ function toggleTheme(){
 
 window.addEventListener('load', initPage);
 
-const cursor = document.getElementById("cursor");
+/* ── CUSTOM CURSOR ── */
+
+const cursor = document.createElement("div");
+cursor.classList.add("custom-cursor");
+document.body.appendChild(cursor);
 
 document.addEventListener("mousemove", (e) => {
   cursor.style.left = e.clientX + "px";
   cursor.style.top = e.clientY + "px";
 });
-const links = document.querySelectorAll("a, button");
 
-links.forEach(el => {
+const hoverElements = document.querySelectorAll("a, button, .char-card, .card-item, .map-pin");
+
+hoverElements.forEach(el => {
+
   el.addEventListener("mouseenter", () => {
-    cursor.style.transform = "translate(-50%, -50%) scale(1.5)";
+    cursor.classList.add("active");
   });
 
   el.addEventListener("mouseleave", () => {
-    cursor.style.transform = "translate(-50%, -50%) scale(1)";
+    cursor.classList.remove("active");
   });
+
 });
